504c056cc7bdb0a123280f5adc37180e89b6da97
Huy Bui <huyb@huyb-macbookpro2.roam.corp.google.com>
2020-03-25 11:06:01 -0700

    add freemarker links/plain-text support and export to zip

--- build.js
+++ build.js
@@ -3,39 +3,15 @@
  * @ nunjucks - templating engine
  * @ fs - file system to write nunjucks rendered html to file
  */
-const Freemarker = require('freemarker');
+const Freemarker = require('freemarker'); /* !!!! not used in this version !!!! */
 const Nunjucks = require('nunjucks');
 const fs = require('fs');
-const path = require('path');
-
-/**
- * Parse YAML data
- * will be replaced by copy on live build
- */
 const yaml = require('js-yaml');
-
-let fileContents = fs.readFileSync('./src/copy/en-US.yml', 'utf8');
-let data = yaml.load(fileContents);
-
-/**
- * @ htmlPath - file path that we want to output the staging html file
- * @ htmlPath/index.html - this is the same file gulp will use to run inline-css and premailer
- * @ nunjucks.configure - set parent directory for nunjucks engine
- * @ htmlString - returns an html string with rendered nunjucks data
- */
-const htmlPath = './src/stage/index.html';
-
-Nunjucks.configure('.');
-const nunjucksHTML = Nunjucks.render('./src/index.html', {
-  n: 1,
-  copy: data
-});
 
 /**
  * @ htmlEscapes - list of HTML escape characters
  * @ htmlEscaper - regex pattern containing the keys listed immediately above in htmlEscapes
  * @ escapeHTML - helper function to run on rendered nunjucksHTML string
- * @ fs.writeFile - writes the nunjucks html string to the file path specified in htmlPath
  */
 const htmlEscapes = {
   '&amp;': '&',
@@ -46,7 +22,16 @@ const htmlEscapes = {
   '&#x2F;': '/'
 };
 
-const htmlEscaper = /(&amp;)|(&lt;)|(&gt;)|(&quot;)|(&#x27;)|(&#x2F;)/ig;
+const mqEscapes = {
+  '&amp;': '&',
+  '&lt;': '<',
+  '&gt;': '>',
+  '&quot;': '"',
+  '&#x27;': "'",
+  '&#x2F;': '/'
+};
+
+const htmlEscaper = /(&amp;)|(&lt;)|(&gt;)|(&quot;)|(&#x27;)|(&#x2F;)/gi;
 
 const escapeHTML = string => {
   return ('' + string).replace(htmlEscaper, match => {
@@ -54,8 +39,59 @@ const escapeHTML = string => {
   });
 };
 
-fs.writeFile(htmlPath, escapeHTML(nunjucksHTML), err => {
-  return err
-    ? console.log(`Error saving file: See exception (${err})`)
-    : console.log('The file was saved successfully!');
+/**
+ * @ nunjucks.configure - set parent directory for nunjucks engine
+ * @ fs.readdir - get all copy files directory
+ * @ copyDirPath - file path to the copies directory
+ */
+Nunjucks.configure('.');
+const copyDirPath = './src/copy';
+
+// LOCALIZED DATA
+const copyData = [];
+const localHTML = [];
+const pt = [];
+
+fs.readdir(copyDirPath, async (err, files) => {
+  // handling error
+  if (err) {
+    console.log(`Unable to scan directory: ${err.message}`);
+  }
+
+  // loop through all files and add contents to arrays: copyData, localHTML, pt
+  const langs = [];
+  await files.forEach(file => {
+    const yamlData = fs.readFileSync(`${copyDirPath}/${file}`, 'utf8');
+    const localCopy = yaml.safeLoad(yamlData);
+    langs.push(file.replace('.yml', ''));
+    copyData.push(localCopy);
+    const html = Nunjucks.render(`./src/index.html`, {
+      n: '${manage_prefs()}',
+      copy: localCopy,
+    });
+    const textData = Nunjucks.render(`./src/plain.html`, {
+      copy: localCopy
+    })
+    pt.push(textData);
+    localHTML.push(html);
+  });
+
+  // loop through html and plain-text content and write files to staging folder ./src/stage
+  for (let i = 0; i < localHTML.length; i++) {
+    // html
+    fs.writeFile(`./src/stage/Default.${langs[i]}.html`, escapeHTML(localHTML[i]), err => {
+      return err
+        ? console.log(`Error saving file: See exception (${err.message})`)
+        : console.log(`The ${langs[i]} file was saved successfully!`);
+    });
+    // plain text
+    fs.writeFile(`./src/stage/Default.${langs[i]}.txt`, escapeHTML(pt[i]), err => {
+      return err
+        ? console.log(`Error saving file: See exception (${err.message})`)
+        : console.log(`The ${langs[i]} plain text file was saved successfully!`);
+    });
+  }
 });
+
+// export copyData if need to use elsewhere
+module.exports = copyData;
--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,68 @@
 const gulp = require('gulp');
 const premailer = require('gulp-premailer');
 const inlineCss = require('gulp-inline-css');
+const fs = require('fs');
+const { zip } = require('zip-a-folder');
+const path = require('path');
 
 /**
  * set task
  * @ build - compiles a final version of index.html from ./src/stage/ and outputs it into ./src/build/
  * @ all string arguments can be modified as needed
  */
-gulp.task('build', function() {
+gulp.task('build-html', () => {
+  return gulp
+    .src('./src/stage/*.html')
+    .pipe(premailer())
+    .pipe(inlineCss({ preserveMediaQueries: true }))
+    .pipe(gulp.dest('./src/build/'));
+});
+
+gulp.task('build-pt', () => {
+  return gulp
+    .src('./src/stage/*.txt')
+    .pipe(gulp.dest('./src/build/'));
+});
+
+
+gulp.task('compile', async () => {
+  const linksFile = './src/partial/links.ftl';
+  await fs.readdir('./src/build', (err, files) => {
+    // handling error
+    if (err) {
+      console.log(`Unable to scan directory: ${err.message}`);
+    }
+    // we only want html files here
+    const htmlFiles = files.filter(el => /\.html$/.test(el))
+    // do something with your files, by the way they are just filenames...
+    console.log(htmlFiles)
+    // loop through html files in directory and prepend freemarker links
+    htmlFiles.forEach(async htmlFile => {
+      const originalFile = `./src/build/${htmlFile}`;
+      const fileData = await fs.readFileSync(originalFile).toString();
+      const prependData = `${await fs.readFileSync(linksFile).toString()} \n\n ${fileData}`;
+      await fs.writeFile(originalFile, prependData, err => {
+        return err
+          ? console.log(`Error saving file: See exception (${err.message})`)
+          : console.log(
+            `File compiled successfully! HTML data for GAMMA has been sent to ${originalFile}`
+          );
+      });
+    });
+  });
+});
+
+gulp.task('zip', async () => {
+  class zipDir {
+    static async main() {
+      await zip('./src/build', '../archive.zip');
+      console.log(`Zip file is located in dir: './../archive.zip'`)
+    }
+  }
+  zipDir.main();
+});
+
+gulp.task('localize', () => {
   return gulp
     .src('./src/stage/*.html')
     .pipe(premailer())
